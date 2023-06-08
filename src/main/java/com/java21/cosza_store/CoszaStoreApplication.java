41c51ff893838133bd2961ff31c3a53d6654ad76
xiucaliu <lelinh050896@gmail.com>
2023-06-08 19:33:35 +0700

    first commit

---
+++ src/main/java/com/java21/cosza_store/CoszaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.java21.cosza_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoszaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoszaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java21/cosza_store/CoszaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java21.cosza_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoszaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
